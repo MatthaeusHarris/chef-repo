c846157e4ea654711703df54408b8ded7bac0b9a
Matt Harris <matthaeus.harris@gmail.com>
2014-01-15 23:38:24 -0800

    Add splunkforwarder home attribute

--- cookbooks/ubersplunk/attributes/default.rb
+++ cookbooks/ubersplunk/attributes/default.rb
@@ -1,4 +1,5 @@
 default[:splunkforwarder][:forward_server] = 		"splunk:9997"
+default[:splunkforwarder][:home] =					"/opt/splunkforwarder"
 default[:splunkforwarder][:bin] = 					"/opt/splunkforwarder/bin/splunk"
 default[:splunkforwarder][:inputs][:chef_log] =		{:file => "/var/log/chef/client.log",
 											:index => "chef"}
