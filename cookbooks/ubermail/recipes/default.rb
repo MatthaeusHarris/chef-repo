19acc05741c1d3f89bcb69327d807077e90c6c5c
Matt Harris <mharris@splunk.com>
2014-03-07 19:52:34 -0800

    Add packages, create mail directory

--- cookbooks/ubermail/recipes/default.rb
+++ cookbooks/ubermail/recipes/default.rb
@@ -5,6 +5,18 @@
 # Copyright 2014, Matt Harris
 #
 
-apt_package "postfix" do 
-	action		:install
+%W(postfix fam courier-pop-ssl courier-imap-ssl 
+	courier-ssl courier-pop courier-imap courier-base
+	libsasl2-2 libsasl2-modules libsasl2-modules-sql 
+	openssl).each do |package|
+	apt_package package do 
+		action		:install
+	end
+end
+
+directory "/mail" do
+	owner		"postfix"
+	group		"postfix"
+	mode 		"0755"
+	action	:create
 end
