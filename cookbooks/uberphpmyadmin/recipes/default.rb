206ebfd4bfdcfe5ff20306259f8329fadf6b82a7
Matt Harris <mharris@splunk.com>
2014-03-13 20:27:01 -0700

    Tweaks to the phpmyadmin recipe

--- cookbooks/uberphpmyadmin/recipes/default.rb
+++ cookbooks/uberphpmyadmin/recipes/default.rb
@@ -5,6 +5,20 @@
 # Copyright 2014, Matt Harris
 #
 
-package "phpmyadmin" do 
-	action		:install
+include_recipe 'apache2::default'
+include_recipe 'apache2::mod_php5'
+include_recipe 'database::mysql'
+include_recipe 'mysql::server'
+
+packages = %w(php5-mysql phpmyadmin)
+
+packages.each do |pkg|
+	package pkg do 
+		action		:install
+	end
+end
+
+link "/etc/apache2/conf.d/phpmyadmin.conf" do
+	to		"/etc/phpmyadmin/apache.conf"
+	notifies	:reload, "service[apache2]"
 end
